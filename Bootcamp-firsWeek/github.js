79e43e6706c0cf05a5477bb966c7089dc9bc4931
brkeozbk <brkeozbk@gmail.com>
2021-09-25 00:55:40 +0300

    First Week assigment

---
+++ Bootcamp-firsWeek/github.js
@@ -0,0 +1,21 @@
+class Github{ //github api json dosyamızın veri çekmemizi sağlayan class
+    constructor(){
+        this.url = "https://api.github.com/users/";
+    }
+    async getGithubData(username){
+        const responseUser = await fetch(this.url+username);
+        const responseRepo = await fetch(this.url+username + "/repos"); //user repos sayfasına yönelen kısım
+        
+
+        const userData = await responseUser.json(); //user bilgilerine eriştiğimiz kod
+        const repoData = await responseRepo.json(); //repo bilgilerimize eriştiğimiz kısım
+
+
+        return{
+            user:userData, 
+            repo:repoData
+           
+        }
+
+    }
+}
